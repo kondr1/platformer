1393e5f975468bdb0be44e15817fcfe2878e1e85
Kondr <i@kondr1.ru>
2019-11-22 21:39:46 +0300

    сменил тайлсет и чутка доработал персонажа

--- main/go/player.go
+++ main/go/player.go
@@ -1,11 +1,6 @@
-embedded_components {
-  id: "sprite"
-  type: "sprite"
-  data: "tile_set: \"/main/global.atlas\"\n"
-  "default_animation: \"wall\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
-  ""
+components {
+  id: "player"
+  component: "/main/go/player.script"
   position {
     x: 0.0
     y: 0.0
@@ -26,14 +21,15 @@ embedded_components {
   "mass: 0.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
-  "group: \"default\"\n"
-  "mask: \"default\"\n"
+  "group: \"player\"\n"
+  "mask: \"ground\"\n"
+  "mask: \"platform\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
   "    position {\n"
   "      x: 0.0\n"
-  "      y: 0.0\n"
+  "      y: -2.0\n"
   "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
@@ -45,13 +41,33 @@ embedded_components {
   "    index: 0\n"
   "    count: 3\n"
   "  }\n"
-  "  data: 150.0\n"
-  "  data: 150.0\n"
+  "  data: 6.0\n"
+  "  data: 12.0\n"
   "  data: 10.0\n"
   "}\n"
   "linear_damping: 0.0\n"
   "angular_damping: 0.0\n"
   "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/assets/Industrial pack/engineer character/engineer.tilesource\"\n"
+  "default_animation: \"idle\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
   ""
   position {
     x: 0.0
